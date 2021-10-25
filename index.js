a2749be254936872225c8118f84e718defd9ae37
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-24 18:11:31 -0700

    Stuff

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+})
 function renderCards(ninja){
     let card = document.createElement('li')
     card.className = 'card'
     card.innerHTML = `
-        <img src="${nija.imageUrl}
+        
+        <div class="content">
+         <h4>${ninja.name}<h4>
+         <p>Weapon: ${ninja.weapon} <p>
+        </div>
     `
+    document.querySelector('#turtles-list').appendChild(card)
+
 }
+
+function getNinjaTurtles(){
+    fetch(" http://localhost:3000/Ninja_Turtles")
+    .then(res => res.json())
+    .then(ninjaTurtleData => ninjaTurtleData.forEach(turtle => renderCards(turtle)));
+
+}
+
+function initalize(){
+    getNinjaTurtles()
+
+}
+
+function postTurtle(){
+    const ninjaObj = {
+        name: 'Kevin McElhinney',
+        weapon: 'Assasian Hidden Blade'
+    }
+    console.log(JSON.stringify(ninjaObj))
+
+    fetch('http://localhost:3000/Ninja_Turtles',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ninjaObj)
+    })
+    .then(() => {getNinjaTurtles()})
+
+}
+initalize()
